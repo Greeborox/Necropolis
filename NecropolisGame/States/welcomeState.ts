f5e69dd183166f811ede6146877c414db3496dda
Greebo <Greebo@DESKTOP-HE04II5>
2016-10-15 12:02:23 +0200

    initial setup

---
+++ NecropolisGame/States/welcomeState.ts
@@ -0,0 +1,9 @@
+﻿module Necorpolis {
+    export class welcomeState extends Phaser.State {
+        bg: Phaser.Sprite;
+
+        create() {
+            this.bg = this.game.add.sprite(0, 0, 'gameSheet', 'bg_main.png');
+        }
+    }
+};
---
+++ NecropolisGame/app.ts
@@ -0,0 +1,19 @@
+﻿module Necorpolis {
+    export class NecropolisGame {
+        game: Phaser.Game;
+        constructor() {
+            this.game = new Phaser.Game(1024, 768, Phaser.AUTO, 'game', { create: this.create, preload: this.preload})
+        }
+        preload() {
+            this.game.load.atlasJSONHash('gameSheet', '/Assets/necropolis.png', '/Assets/necropolis.json')
+        }
+        create() {
+            this.game.state.add('welcomeState', Necorpolis.welcomeState, true);
+        }
+        
+    }
+}
+
+window.onload = () => {
+    var game = new Necorpolis.NecropolisGame();
+};
